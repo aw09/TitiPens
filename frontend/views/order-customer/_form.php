e0c2c9cdffa6795dec7222efcca3eddd1eca3f4c
wifda <wifdamu@gmail.com>
2019-06-12 20:19:19 +0700

    pengguna-buat-profile&_form-utk-isi-lokasi-n-catatan

--- frontend/views/order-customer/_form.php
+++ frontend/views/order-customer/_form.php
@@ -1,31 +1,102 @@
 <?php
 
-use yii\helpers\Html;
-use yii\widgets\ActiveForm;
-
 /* @var $this yii\web\View */
-/* @var $model frontend\models\OrderCustomer */
-/* @var $form yii\widgets\ActiveForm */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use yii\helpers\ArrayHelper;
+use frontend\models\OrderCustomer;
+
+
 ?>
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+</head>
+  <style>
+  .login-clean {
+    padding: 80px 0;
+  }
 
-<div class="order-customer-form">
+  .login-clean form {
+    max-width:320px;
+    width:90%;
+    margin:0 auto;
+    background-color:#ffffff;
+    padding:40px;
+    border-radius:4px;
+    color:#505e6c;
+    box-shadow:1px 1px 5px rgba(0,0,0,0.1);
+  }
 
-    <?php $form = ActiveForm::begin(); ?>
+  .login-clean .illustration {
+    text-align:center;
+    padding:0 0 20px;
+    font-size:100px;
+    color:rgb(244,71,107);
+  }
 
-    <!--<?= $form->field($model, 'tanggal')->textInput() ?>-->
+  .login-clean form .form-control {
+    background:#f7f9fc;
+    border:none;
+    border-bottom:1px solid #dfe7f1;
+    border-radius:0;
+    box-shadow:none;
+    outline:none;
+    color:inherit;
+    text-indent:8px;
+    height:42px;
+  }
 
-    <!--<?= $form->field($model, 'user_id')->textInput() ?>-->
+  .login-clean form .btn-primary {
+    background:rgb(248,217,54);
+    border:none;
+    border-radius:4px;
+    padding:11px;
+    box-shadow:none;
+    margin-top:26px;
+    text-shadow:none;
+    outline:none !important;
+  }
 
-    <?= $form->field($model, 'lokasi')->textInput(['maxlength' => true]) ?>
+  .login-clean form .btn-primary:hover, .login-clean form .btn-primary:active {
+    background:rgb(0,0,0);
+  }
 
-    <?= $form->field($model, 'catatan')->textarea(['rows' => 6]) ?>
+  .login-clean form .btn-primary:active {
+    transform:translateY(1px);
+  }
 
-    <?= $form->field($model, 'total')->textInput() ?>
+  .login-clean form .forgot {
+    display:block;
+    text-align:center;
+    font-size:12px;
+    color:#6f7a85;
+    opacity:0.9;
+    text-decoration:none;
+  }
 
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
-    </div>
+  .login-clean form .forgot:hover, .login-clean form .forgot:active {
+    opacity:1;
+    text-decoration:none;
+  }
+  </style>
 
-    <?php ActiveForm::end(); ?>
-
-</div>
+    <body>
+        <div class="login-clean">
+          <?php $form = ActiveForm::begin(); ?>
+              <div class="illustration"><img src="<?= yii\helpers\Url::base().'/gambar/logos.jpg' ?>"></div>
+              <div class="form-group"><?= $form->field($model, 'lokasi')->textInput(['maxlength' => true]) ?></div>
+              <div class="form-group"><?= $form->field($model, 'catatan')->textarea(['rows' => 6]) ?></div>
+              <div class="form-group">
+                  <?= Html::submitButton('Save', ['class' => 'btn btn-primary btn-block']) ?>
+                  <?= Html::a('Cancel', ['index'], ['class'=>'btn btn-primary btn-block']) ?>
+              </div>
+          <?php ActiveForm::end(); ?>
+        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
